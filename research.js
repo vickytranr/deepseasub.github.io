32edba21961f37e30c031388468abe8932a0a77c
vickytranr <78326917+vickytranr@users.noreply.github.com>
2021-02-07 22:57:40 -0500

    added js canvas

--- research.js
+++ research.js
@@ -3,4 +3,19 @@ console.log("research log");
 var canvas;
 var ctx;
 var w = 1300;
-var h = 400;
+var h = 550;
+
+
+setUpCanvas();
+
+
+
+function setUpCanvas(){
+    canvas = document.querySelector("#myCanvas");
+    ctx = canvas.getContext("2d");
+    canvas.width = w;
+    canvas.height = h;
+    canvas.style.border = "50px solid lightsteelblue";
+    // ctx.fillStyle = "lightslategrey";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+}
