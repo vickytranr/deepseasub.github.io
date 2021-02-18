9990fa4da0106ebe9c076e986b706ebce05cc516
vickytranr <78326917+vickytranr@users.noreply.github.com>
2021-02-17 23:36:46 -0500

    lots of new changes, js, revamped css

---
+++ navigation.js
@@ -0,0 +1,15 @@
+function control() {
+    document.getElementById("controlButton").innerHTML = "CONTROL ROOM: Read the submarine's introduction again!";
+}
+
+function navigation() {
+    document.getElementById("navButton").innerHTML = "NAVIGATION ROOM: This is where you are. See information about all the rooms here!";
+}
+
+function observation() {
+    document.getElementById("obsButton").innerHTML = "OBSERVATION DECK: Read some of the confessions people have shared. Updated periodically.";
+}
+
+function communication() {
+    document.getElementById("commButton").innerHTML = "COMMUNICATION ROOM: Anonymously let a message out to the sea!";
+}
