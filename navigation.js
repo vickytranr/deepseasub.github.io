471f218b50a6fa5e72b5bfcaa57d9314f69d58a9
vickytranr <78326917+vickytranr@users.noreply.github.com>
2021-02-28 18:50:47 -0500

    lots of tweaks!!! parallax edit, css stuff, new storage

--- navigation.js
+++ navigation.js
@@ -1,15 +1,15 @@
 function control() {
-    document.getElementById("controlButton").innerHTML = "CONTROL ROOM: Read the submarine's introduction again!";
+    document.getElementById("controlButton").innerHTML = "CONTROL ROOM: This room is how we move the submarine!<br>You can read the submarine's introduction, and the house rules here.";
 }
 
 function navigation() {
-    document.getElementById("navButton").innerHTML = "NAVIGATION ROOM: This is where you are. See information about all the rooms here!";
+    document.getElementById("navButton").innerHTML = "NAVIGATION ROOM: This is where you are.<br>See information about all the rooms here!<br>We use this room to map out the sea and see if there is anything of interest in the area.";
 }
 
 function observation() {
-    document.getElementById("obsButton").innerHTML = "OBSERVATION DECK: Read some of the confessions people have shared. Updated periodically.";
+    document.getElementById("obsButton").innerHTML = "OBSERVATION DECK: Read some of the confessions people have<br>shared from the COMMUNICATION ROOM!<br>The sea is always moving, so messages can change over time.";
 }
 
 function communication() {
-    document.getElementById("commButton").innerHTML = "COMMUNICATION ROOM: Anonymously let a message out to the sea!";
+    document.getElementById("commButton").innerHTML = "COMMUNICATION ROOM: Anonymously let a message out to the sea!<br>Our radios allow for a short message to be projected into the sea,<br>and they are all manually and periodically updated.";
 }
