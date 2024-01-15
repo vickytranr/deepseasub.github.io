7b500f5b2bc8301c6f8a79c4894f8866a6f817fa
vickytranr <78326917+vickytranr@users.noreply.github.com>
2024-01-15 16:37:47 -0500

    new nav

--- navigation.js
+++ navigation.js
@@ -1,15 +1,15 @@
 function control() {
-    document.getElementById("controlButton").innerHTML = "CONTROL ROOM: This room is how we move the submarine!<br>You can read the submarine's introduction, and the house rules here.";
+    document.getElementById("controlButton").innerHTML = "This room is how we move the submarine! You can read the submarine's introduction, and the house rules here.";
 }
 
 function navigation() {
-    document.getElementById("navButton").innerHTML = "NAVIGATION ROOM: This is where you are.<br>See information about all the rooms here!<br>We use this room to map out the sea and see if there is anything of interest in the area.";
+    document.getElementById("navButton").innerHTML = "This is where you are. See information about all the rooms here! We use this room to map out the sea and see if there is anything of interest in the area.";
 }
 
 function observation() {
-    document.getElementById("obsButton").innerHTML = "OBSERVATION DECK: Read some of the confessions people have<br>shared from the COMMUNICATION ROOM!<br>The sea is always moving, so messages can change over time.";
+    document.getElementById("obsButton").innerHTML = "Read some of the confessions people have shared from the COMMUNICATION ROOM! The sea is always moving, so you might see some new messages through the water.";
 }
 
 function communication() {
-    document.getElementById("commButton").innerHTML = "COMMUNICATION ROOM: Anonymously let a message out to the sea!<br>Our radios allow for a short message to be projected into the sea,<br>and they are all manually and periodically updated.";
+    document.getElementById("commButton").innerHTML = "Anonymously let a message out to the sea! Our radios allow for a short message to be projected into the sea,<br>and they are all manually and periodically updated.";
 }
